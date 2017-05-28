dd664da9e4d411c4d38351f8714816274c324dda
jeewendra kumar <jeewendrakumar@gmail.com>
2017-05-28 17:41:07 +0530

    Add files via upload

---
+++ app/js/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class App extends React.Component {
+
+    render() {
+        debugger;
+        console.log("test");
+        return (
+            <div className="app container">
+                <div className="hello-text bold red">Hello World!</div>
+                <h1>Hello World with Twitter Bootstrap!</h1>
+            </div>
+        );
+    }
+}
